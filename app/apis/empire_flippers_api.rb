791c10af7b881a5a5860dc5f11601fb5e6e8f3ee
Emmanuel Natividad <emmanuel@natividad.info>
2021-06-24 10:34:27 +0800

    + simple rate limit mechanism for EmpireFlippersApi

--- app/apis/empire_flippers_api.rb
+++ app/apis/empire_flippers_api.rb
@@ -2,6 +2,23 @@
 module EmpireFlippersApi
   BASE_URL = 'https://api.empireflippers.com/api/v1/'
 
+  def self.rate_limit
+    # 0.0 because using mocks to speed up testing
+    return 0.0 if Rails.env.test?
+
+    # as stated in: https://empireflippers.com/empire-flippers-public-listings-api/
+    # "Please make no more than 1 request per second."
+    1.0
+  end
+
+  def self.calc_rate_limit_sleep_duration
+    past = Time.now
+    yield
+    request_duration = Time.now - past
+    sleep_duration = rate_limit - request_duration
+    [0.0, sleep_duration].max
+  end
+
   module Listing
     def self.list(**params)
       response = Faraday.get(BASE_URL + 'listings/list', params)
